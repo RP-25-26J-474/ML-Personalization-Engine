b90a772b180594484c293e063c3ffff3fbfca027
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-12-29 20:36:49 +0530

    feat: Basic Temporary User Detector page Created

--- frontend/src/pages/CategoryEngine.jsx
+++ frontend/src/pages/CategoryEngine.jsx
@@ -11,11 +11,11 @@ export default function CategoryEngine() {
           <div className="grid h-full min-h-0 grid-cols-12 gap-3">
             <div className="col-span-12 xl:col-span-8 flex min-h-0 flex-col gap-3">
               <div className="grid min-h-0 flex-1 grid-cols-12 gap-3">
-                <div className="col-span-12 xl:col-span-6 bg-base-200 rounded-lg shadow border-2 border-primary/70 flex min-h-0 flex-col">
+                <div className="col-span-12 xl:col-span-6 bg-base-200 rounded-lg shadow border-2 border-primary/70 flex min-h-60 flex-col">
                   <InputSection />
                 </div>
 
-                <div className="col-span-12 xl:col-span-6 bg-base-200 rounded-lg shadow border-2 border-primary/70 flex min-h-0 flex-col">
+                <div className="col-span-12 xl:col-span-6 bg-base-200 rounded-lg shadow border-2 border-primary/70 flex min-h-60 flex-col">
                   <OutputSection />
                 </div>
               </div>
